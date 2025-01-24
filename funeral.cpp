a8bfec303f96200865b577a4f00f7cd4d6237107
AObernihin <arsenobernihin@gmail.com>
2025-01-25 00:35:03 +0200

    2347

--- funeral.cpp
+++ funeral.cpp
@@ -41,7 +41,7 @@ public:
     void DeleteFromTail()
     {
         if (IsEmpty()) {
-            throw exception("List is empty"); // ТУТ ВИНЯТОК ДЗ
+            throw exception("List is empty"); // ТУТ ВИНЯТОК ДЗ n1
         }
 
         if (head->next == nullptr)
@@ -90,7 +90,7 @@ public:
 
     void SetSize(int size, int grow) {
         if (size == GetSize() || size == 0) {
-            throw exception("size is the same or 0"); // ТУТ ВИНЯТОК ДЗ
+            throw exception("size is the same or 0"); // ТУТ ВИНЯТОК ДЗ n1
         }
         if (size > GetSize()) {
             for (int i = 0; i < size - GetSize() - grow; i++) {
@@ -145,7 +145,7 @@ public:
 
     void SetAt(int index, int value) {
         if (index >= GetSize()) {
-            throw exception("Index out of range"); // ТУТ ВИНЯТОК ДЗ
+            throw exception("Index out of range"); // ТУТ ВИНЯТОК ДЗ n1
         }
 
         int count = 0;
@@ -235,7 +235,7 @@ public:
 
     void DeleteFromHead() {
         if (IsEmpty()) {
-            throw exception("List is empty"); // ТУТ ВИНЯТОК ДЗ
+            throw exception("List is empty"); // ТУТ ВИНЯТОК ДЗ n1
         }
 
         if (head != nullptr) {
@@ -252,6 +252,68 @@ public:
         Clear();
     }
 };
+
+//===========================================================
+
+class drib
+{
+public:
+    int znamenik;
+    int chiselnik;
+
+public:
+    void setZnamenik(int _znamenik)
+    {
+        if (_znamenik == 0) throw exception("Division by zero"); // ТУТ ВИНЯТОК ДЗ n2
+        znamenik = _znamenik;
+    }
+
+    void setChiselnik(int _chiselnik)
+    {
+        chiselnik = _chiselnik;
+    }
+
+    drib operator+(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.znamenik;
+        result.chiselnik = this->chiselnik * other.znamenik + other.chiselnik * this->znamenik;
+        return result;
+    }
+
+    drib operator-(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.znamenik;
+        result.chiselnik = this->chiselnik * other.znamenik - other.chiselnik * this->znamenik;
+        return result;
+    }
+
+    drib operator*(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.znamenik;
+        result.chiselnik = this->chiselnik * other.chiselnik;
+        return result;
+    }
+
+    drib operator/(const drib other) const
+    {
+        if(other.znamenik == 0) throw exception("Division by zero"); // ТУТ ВИНЯТОК ДЗ n2
+        if(other.chiselnik == 0) throw exception("Division by zero"); // ТУТ ВИНЯТОК ДЗ n2
+        drib result;
+        result.znamenik = this->znamenik * other.chiselnik;
+        result.chiselnik = this->chiselnik * other.znamenik;
+        return result;
+    }
+
+};
+
+
+
+
+
+
 
 int main()
 {
